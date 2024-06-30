fbcd0d85b3194c6e78bbba885030302c8e6017ce
hhplum <hhplum6@gmail.com>
2024-06-30 21:36:15 +0800

    feat(dependencies): Add eslint

---
+++ eslint.config.js
@@ -0,0 +1,177 @@
+// @ts-check
+import { builtinModules } from 'node:module'
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginN from 'eslint-plugin-n'
+import pluginImportX from 'eslint-plugin-import-x'
+import * as pluginRegexp from 'eslint-plugin-regexp'
+// import vitest from 'eslint-plugin-vitest'
+import globals from 'globals'
+
+export default tseslint.config(
+  {
+    ignores: [
+      '**/dist/',
+      'docs/.vitepress/',
+      '.idea/',
+      '.git/',
+      'node_modules',
+      '.rollup.cache/',
+    ],
+  },
+
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.stylistic,
+  pluginRegexp.configs['flat/recommended'],
+
+  {
+    name: 'main',
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 2021,
+      },
+      globals: {
+        ...globals.es2021,
+      },
+    },
+    plugins: {
+      'import-x': pluginImportX,
+    },
+    rules: {
+      eqeqeq: ['warn', 'always', { null: 'never' }],
+      'no-debugger': ['error'],
+      'no-empty': ['warn', { allowEmptyCatch: true }],
+      'no-process-exit': 'off',
+      'no-useless-escape': 'off',
+      'prefer-const': [
+        'warn',
+        {
+          destructuring: 'all',
+        },
+      ],
+
+      'no-extra-semi': 'off',
+      '@typescript-eslint/ban-ts-comment': 'error',
+      '@typescript-eslint/ban-types': 'off', // TODO: we should turn this on in a new PR
+      '@typescript-eslint/explicit-module-boundary-types': [
+        'error',
+        { allowArgumentsExplicitlyTypedAsAny: true },
+      ],
+      '@typescript-eslint/no-empty-function': [
+        'error',
+        { allow: ['arrowFunctions'] },
+      ],
+      '@typescript-eslint/no-empty-interface': 'off',
+      '@typescript-eslint/no-explicit-any': 'off', // maybe we should turn this on in a new PR
+      '@typescript-eslint/no-extra-semi': 'off', // conflicts with prettier
+      '@typescript-eslint/no-inferrable-types': 'off',
+      '@typescript-eslint/no-unused-vars': 'off', // maybe we should turn this on in a new PR
+      '@typescript-eslint/no-var-requires': 'off',
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        { prefer: 'type-imports', disallowTypeAnnotations: false },
+      ],
+      // disable rules set in @typescript-eslint/stylistic v6 that wasn't set in @typescript-eslint/recommended v5 and which conflict with current code
+      // maybe we should turn them on in a new PR
+      '@typescript-eslint/array-type': 'off',
+      '@typescript-eslint/ban-tslint-comment': 'off',
+      '@typescript-eslint/consistent-generic-constructors': 'off',
+      '@typescript-eslint/consistent-indexed-object-style': 'off',
+      '@typescript-eslint/consistent-type-definitions': 'off',
+      '@typescript-eslint/prefer-for-of': 'off',
+      '@typescript-eslint/prefer-function-type': 'off',
+
+      'import-x/no-duplicates': 'error',
+      'import-x/order': 'error',
+      'sort-imports': [
+        'error',
+        {
+          ignoreCase: false,
+          ignoreDeclarationSort: true,
+          ignoreMemberSort: false,
+          memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+          allowSeparatedGroups: false,
+        },
+      ],
+
+      'regexp/no-contradiction-with-assertion': 'error',
+      // in some cases using explicit letter-casing is more performant than the `i` flag
+      'regexp/use-ignore-case': 'off',
+    },
+  },
+
+  {
+    name: 'web',
+    files: ['packages/browser/**/*', 'packages/global/**/*'],
+    ignores: ['**/__tests__/**/*'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+
+  {
+    name: 'node',
+    files: [
+      'packages/node/**/*',
+      'packages/test/**/*',
+      '**/__tests__/**/*',
+      'build/**/*',
+      'scripts/**/*',
+      'rollup.config.js',
+      'vitest.config.ts',
+    ],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+    plugins: {
+      n: pluginN,
+    },
+    rules: {
+      'n/no-exports-assign': 'error',
+      'n/no-unpublished-bin': 'error',
+      'n/no-unsupported-features/es-builtins': 'error',
+      'n/no-unsupported-features/node-builtins': 'error',
+      'n/process-exit-as-throw': 'error',
+      'n/hashbang': 'error',
+
+      'n/no-missing-require': [
+        'error',
+        {
+          // for try-catching yarn pnp
+          allowModules: ['vite'],
+          tryExtensions: ['.ts', '.js', '.d.ts'],
+        },
+      ],
+      'n/no-extraneous-import': [
+        'error',
+        {
+          allowModules: [
+            'vite',
+            'vitest',
+            '@hhplum/utils-global',
+            '@hhplum/utils-node',
+            '@hhplum/utils-test',
+          ],
+        },
+      ],
+      'n/no-extraneous-require': [
+        'error',
+        {
+          allowModules: ['vite'],
+        },
+      ],
+
+      'import-x/no-nodejs-modules': [
+        'error',
+        { allow: builtinModules.map(mod => `node:${mod}`) },
+      ],
+    },
+  },
+)
